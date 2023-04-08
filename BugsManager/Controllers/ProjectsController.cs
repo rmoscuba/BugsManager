2ba999a47fb091eb29eb86024d57549664a1cba5
Rodolfo Ortega <rmoscuba@gmail.com>
2023-04-08 05:06:26 -0400

    Add Users and Projects GET

---
+++ BugsManager/Controllers/ProjectsController.cs
@@ -0,0 +1,39 @@
+﻿using BugsManager.Contexts;
+using BugsManager.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using BugsManager.Interfaces;
+using Microsoft.AspNetCore.Http;
+
+namespace BugsManager.Controllers
+{
+
+    [Route("[controller]")]
+    [ApiController]
+    public class ProjectsController : ControllerBase
+    {
+        private IRepositoryManager _repository;
+
+        public ProjectsController(IRepositoryManager repository)
+        {
+            _repository = repository;
+        }
+
+
+        // GET: /<UsersController>
+        [HttpGet]
+        public IActionResult Get([FromQuery] Guid? project_id)
+        {
+            var projects = _repository.Project.GetAllProjects(trackChanges: false)
+                    .Where(u => project_id is null ? true: u.Id == project_id );
+
+            if (!projects.Any()) { return NotFound(); }
+            else { return Ok(new { projects = projects }); }
+        }
+    }
+}
---
+++ BugsManager/Controllers/UsersController.cs
@@ -0,0 +1,47 @@
+﻿using BugsManager.Contexts;
+using BugsManager.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using BugsManager.Interfaces;
+using Microsoft.AspNetCore.Http;
+
+namespace BugsManager.Controllers
+{
+
+    [Route("[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private IRepositoryManager _repository;
+
+        public UsersController(IRepositoryManager repository)
+        {
+            _repository = repository;
+        }
+
+
+        // GET: /<UsersController>
+        [HttpGet]
+        public IActionResult Get([FromQuery] Guid? user_id)
+        {
+            var users = _repository.User.GetAllUsers(trackChanges: false)
+                    .Where(u => user_id is null ? true: u.Id == user_id )
+                    .Select(u =>
+                    {
+                        return new
+                        {
+                            user = u.Id,
+                            name = u.Name
+                        };
+                    });
+
+            if (!users.Any()) { return NotFound(); }
+            else { return Ok(new { users = users }); }
+        }
+    }
+}
